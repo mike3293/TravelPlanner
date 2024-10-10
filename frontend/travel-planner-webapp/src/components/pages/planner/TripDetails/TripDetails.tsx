164f55b667a3d3f6f9d294e8eb80bb2233a60d8d
Mikhail Gorodilov <misha5555573@gmail.com>
2024-10-10 21:57:30 +0200

    Call refresh one

--- frontend/travel-planner-webapp/src/components/pages/planner/TripDetails/TripDetails.tsx
+++ frontend/travel-planner-webapp/src/components/pages/planner/TripDetails/TripDetails.tsx
@@ -2,10 +2,16 @@ import { Typography } from '@mui/material';
 import { useParams } from 'react-router-dom';
 
 import styles from './TripDetails.module.scss';
+import { useEffect } from 'react';
+import { tripsService } from 'src/config/services';
 
 
 export function TripDetails() {
     const { tripId } = useParams();
+
+    useEffect(() => {
+        tripsService.refreshSession();
+    }, []);
 
     return (
         <div className={styles.tripList}>
--- frontend/travel-planner-webapp/src/services/serviceBase/ServiceWithAuthBase.ts
+++ frontend/travel-planner-webapp/src/services/serviceBase/ServiceWithAuthBase.ts
@@ -13,7 +13,6 @@ export abstract class ServiceWithAuthBase extends ServiceBase {
 
 
     async willSendRequest(request: RequestInit) {
-        await this.refreshSession();
         let accessToken = this.store.getState().accessToken;
 
         if (accessToken) {
